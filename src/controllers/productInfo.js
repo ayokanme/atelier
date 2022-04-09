41aa31fb92a9fb89e9addb1ae0e288a682427c4d
Muizz Matemilola <88415295+ayokanme@users.noreply.github.com>
2022-04-09 17:18:33 -0500

    implemented /products/:product_id server endpoint

--- src/controllers/productInfo.js
+++ src/controllers/productInfo.js
@@ -1,7 +1,38 @@
-// import { Product } from '../db.js';
+/* eslint-disable import/extensions */
+import Product from '../db.js';
 
-const productInfo = (req, res) => {
-  res.json('big data about the Product Info');
+const productInfo = async (req, res) => {
+  const productId = Number(req.params.product_id);
+
+  if (Number.isNaN(productId)) {
+    res.status(404).send('wrong parameter type');
+  }
+
+  try {
+    const productData = await Product.aggregate([
+      {
+        $match: {
+          id: productId,
+        },
+      },
+      {
+        $project: {
+          _id: false,
+          styles: false,
+          related: false,
+        },
+      },
+    ]);
+
+    if (productData.length > 0) {
+      res.status(200).json(productData);
+    } else {
+      res.status(400).send('product with that ID does not exist');
+    }
+  } catch (error) {
+    console.log('server error: ', error);
+    res.status(500).send('server error');
+  }
 };
 
 export default productInfo;
